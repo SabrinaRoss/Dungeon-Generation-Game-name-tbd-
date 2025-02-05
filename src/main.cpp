9c5ddaa9128872a4516952d8b91ceb5ad097d5d9
Rowan Althorp <rowalthorp@gmail.com>
2025-02-04 16:23:54 -0800

    got started working on textbox and buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "ui/textbox.hpp"
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Dungeon Crawler");
+    sf::RenderWindow w;
+    sf::Vector2f centerWindow((sf::VideoMode::getDesktopMode().width / 2)-445, (sf::VideoMode::getDesktopMode().height / 2) - 480);
+    w.create(sf::VideoMode(800, 600), "Dungeon Crawler", sf::Style::Titlebar | sf::Style::Close);
+    w.setPosition(sf::Vector2i(centerWindow.x, centerWindow.y));
+    w.setKeyRepeatEnabled(true);
 
     //this space instantiate all classes need
-
     sf::Clock clock;
     float delta_time {0.0};
-    
-    sf::Event event;
-    while (window.isOpen()) {
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed) window.close(); //change this to specific button start game
-    
-    //window.clear(sf::Color::Black);
-        window.display(); 
+    Textbox textbox1(15, sf::Color::White, true);
+   
+    while (w.isOpen()) {
+        sf::Event event;
+        while (w.pollEvent(event)) {
+            switch(event.type) {
+
+                case sf::Event::Closed:
+                    w.close();
+                    break;
+            }
+        }
     }
-    return 0;
+    w.clear();
+    textbox1.drawTwo(w);
+    w.display();
 }
---
+++ src/ui/textbox.hpp
@@ -0,0 +1,75 @@
+// This code took lots of inspiration from this video: https://www.youtube.com/watch?v=T31MoLJws4U
+#include <SFML/Graphics.hpp>
+#include <sstream>
+
+#define DELETE_KEY 8
+#define ENTER_KEY 13
+#define ESCAPE_KEY 27
+// change the defines laster, I do not like defines
+
+class Textbox {
+public:
+    Textbox() { }
+    Textbox(int size, sf::Color colour, bool sel) {
+        textbox.setCharacterSize(size);
+        textbox.setColour(colour);
+        is_selected = sel;
+        if (sel) textbox.toString(" ");
+        else textbox.toString("");
+    }
+    void setFont(sf::Font &font) textbox.setFont(font);
+    void setPosition(sf::Vector2f pos) textbox.setPosition(pos);
+    void setLimit(bool tf) has_limit = tf;
+    void setLimit(bool tf, int lim) {
+        has_limit = tf;
+        limit = lim;
+    }
+    void setSelected(bool sel) {
+        is_selected = sel;
+        if (!sel) {
+            std::string t = text.str();
+            std::string nt = "";
+            int i { 0 };
+            for (; i < t.length() - 1; i++) nt += t[i];
+            textbox.setString(nt);
+        }
+    }
+    std::string getText() return text.str();
+    void drawTwo(sf::RenderWindow &window) window.draw(textbox);
+    void typedOn(sf::Event &input) if (is_selected) {
+        int ct = input.text.unicode;
+        if (ct < 128) {
+            if (has_limit) {
+                if (text.str().length() <= limit) inputLogic(ct);
+                else if (text.str().length() > limit && ct == DELETE_KEY) deleteLastChar();
+            }
+            else inputLogic(ct);
+        }
+    }
+    //passing input by referance, experienctal change 
+private:
+    sf::Text textbox;
+    std::ostringstream text;
+    bool is_selected = false;
+    bool has_limit = false;
+    int limit;
+
+    void inputLogic(int ct) {
+        if (ct != DELETE_KEY && ct != ENTER_KEY && ct != ESCAPE_KEY) text << static_cast<char>(ct)
+        else if (ct == DELETE_KEY) if (text.str().length() > 0) {
+            deleteLastChar();
+        }
+        textbox.setString(text.str() + "_");
+    }
+    void deleteLastChar() {
+        std::string t = text.str();
+        std::string nt = "";
+        int i { 0 };
+        for (; i < t.length() - 1; i++) nt += t[i];
+        text.str("");
+        text << nt;
+        textbox.setString(text.str());
+    }
+
+
+}
