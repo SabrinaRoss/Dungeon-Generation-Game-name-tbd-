720fff77cc96f13a07727200e1d45d0c2895edb2
Rowan Althorp <rowalthorp@gmail.com>
2025-02-25 13:24:04 -0800

    working on procedural generation for the rooms atm

--- src/dungeon/dungeon_generator.cpp
+++ src/dungeon/dungeon_generator.cpp
@@ -1,9 +1,10 @@
-#inlcude "dungeon_generator.h"
+#include "dungeon_generator.h"
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 
 DungeonGenerator::DungeonGenerator(int width, int height)
-    : width(width), height(height) {
+    : area({0, o, width, height}), width(width), height(height) {
 
     tiles.resize(height);
     int j { 0 };
@@ -13,4 +14,13 @@ DungeonGenerator::DungeonGenerator(int width, int height)
         for (; j < width, j++) tiles[i][j] = Tile[i][j] = Tile(j, i, TileType::GROUND); // set this to diffrent when make the algorithm
     }
 }
+~DungeonGenerator() { delete left; delete right; }
 std::vector<std::vector<Tile>>& DungeonGenerator::getTiles() { return tiles; }
+bool DungeonGenerator::isLeaf() const { return left == nullptr && right == nullptr; }
+void DungeonGenerator::generateDungeon() {
+    split(20); //change this later
+    generateRoomsRecursive();
+    connectRoomsRecursive();
+    carveRoomsAndCorridors();
+}
+
--- src/dungeon/dungeon_generator.h
+++ src/dungeon/dungeon_generator.h
@@ -7,12 +7,27 @@
 class DungeonGenerator() {
 public:
     DungeonGenerator(int width, int height);
+    ~DungeonGenerator();
     std::vector<std::vector<Tile>& getTiles();
     void generateDungeon(); //change this later when have the procedural stuff intergrated
+    bool isLeaf() const;
+    void generateRoom();
+    void connectRooms(DungeonGenerator* other);
+    bool split(int min_size);
+    void getRoom() const;
 private:
-    int height;
-    int width;
+    struct Rect {
+        int x { 0 }, y { 0 }, w { 0 }, h { 0 }
+    };
     std::vector<std::vector<Tile>> tiles;
+    Rect area;
+    Rect room;
+    DungeonGenerator* left { nullptr };
+    DungeonGenerator* right { nullptr };
+    bool has_room { false };
+    int randomInt(int min, int max);
+    void createBetweenRooms(int x1, int y1, int x2, int y2);
+
 }
 
 #endif
